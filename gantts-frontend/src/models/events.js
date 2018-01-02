d83344235ca7ab1eedd925bad2ccf8268afaa063
pawlkris <paulkristapovich@gmail.com>
2018-01-02 15:57:08 -0500

    added models

--- gantts-frontend/src/models/events.js
+++ gantts-frontend/src/models/events.js
@@ -1,7 +1,13 @@
-class Model {
+class Event {
   constructor(data){
-    
-  }
+    this.id = data["id"];
+    this.title = data["title"];
+    this.content = data["content"];
+    this.start_time = data["start_time"];
+    this.duration = data["duration"];
+    Event.all.push(this);
+  };
 
+};
 
-}
+Event.all = [];
---
+++ gantts-frontend/src/models/tracks.js
@@ -0,0 +1,11 @@
+class Track {
+  constructor(data){
+    this.id = data["id"];
+    this.priority = data["priority"];
+    Track.all.push(this);
+  };
+
+
+};
+
+Track.all = [];

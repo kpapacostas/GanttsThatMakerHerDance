ca95abe8331f1de536fd9c9fb792dc77f126ba31
jjwumaster <jjwumaster@gmail.com>
2018-01-03 14:21:24 -0500

    added sortable functionality with saving of x axis location when dropped

--- gantts-frontend/src/app.js
+++ gantts-frontend/src/app.js
@@ -8,9 +8,26 @@ class App {
   }
 
 
-  static singleClick() {
-    document.body.addEventListener('click', (e)=>{
-      alert()
+  static mouseUp() {
+    document.body.addEventListener('mouseup', (e)=>{
+      let element = e.target
+      switch(e.target.className) {
+        case "task ui-sortable-handle":
+
+          console.log(element.id)
+
+          let rect = element.getBoundingClientRect()
+          console.log(
+            "Width:" + element.clientWidth + ", " +
+            "Id:" + element.id + ", " +
+            "x:" + rect.left + ", " +
+            "y:" + rect.right)
+          console.log(element.parentElement)
+
+          break
+        default:
+          console.log("ya clicked")
+      }
     })
   }
 }
--- gantts-frontend/src/index.js
+++ gantts-frontend/src/index.js
@@ -1,10 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   // ProjectsAdapter.all()
-<<<<<<< HEAD
 
   App.doubleClick()
-=======
-  App.singleClick()
+  App.mouseUp()
 
->>>>>>> 5b188382f5fe6b21382363082876216b0b80eca1
 })

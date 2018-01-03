7540b8225bce426923854b5334157e866b4fccc4
pawlkris <paulkristapovich@gmail.com>
2018-01-03 15:38:46 -0500

    fixed discrepancies and all is well

--- gantts-frontend/src/app.js
+++ gantts-frontend/src/app.js
@@ -2,34 +2,6 @@ class App {
 
 
 
-  static progressBar(totalTime) {
-    let totalSeconds = totalTime*60;
-    let totalMilliseconds = totalTime*60*1000;
-    $("#myBar").animate({width: '100%'}, totalMilliseconds);
-
-      let myInterval = setInterval(function(){
-        let formattedSeconds = formattedTime(totalSeconds);
-        document.getElementById("myBar").innerText=formattedSeconds;
-        if (totalSeconds > 0){
-      totalSeconds--;
-    } else { clearInterval(myInterval)}
-    }
-        ,1000);
-    };
-
-};
-
-
-//formats time in seconds to min:seconds
-function formattedTime(time) {
-  let mins = 0;
-  let seconds = 0;
-  mins = Math.floor(time/60)
-  seconds = time%60
-  if (String(mins).length<2) mins="0"+String(mins)
-  if (String(seconds).length<2) seconds="0"+String(seconds)
-  return `${mins}:${seconds}`
-};
   static click() {
     document.addEventListener('click', (e) => {
 
@@ -91,4 +63,30 @@ function formattedTime(time) {
   //     }
   //   })
   // }
-}
+
+  static progressBar(totalTime) {
+    let totalSeconds = totalTime*60;
+    let totalMilliseconds = totalTime*60*1000;
+    $("#myBar").animate({width: '100%'}, totalMilliseconds);
+
+      let myInterval = setInterval(function(){
+        let formattedSeconds = formattedTime(totalSeconds);
+        document.getElementById("myBar").innerText=formattedSeconds;
+        if (totalSeconds > 0){
+      totalSeconds--;
+    } else { clearInterval(myInterval)}
+    }
+        ,1000);
+    };
+}; //END OF APP CLASS
+
+//formats time in seconds to min:seconds
+function formattedTime(time) {
+  let mins = 0;
+  let seconds = 0;
+  mins = Math.floor(time/60)
+  seconds = time%60
+  if (String(mins).length<2) mins="0"+String(mins)
+  if (String(seconds).length<2) seconds="0"+String(seconds)
+  return `${mins}:${seconds}`
+};

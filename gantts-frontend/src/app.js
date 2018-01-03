2428827933ad6886162b7a90294e0ae5ad1ee6ab
github username <github email address>
2018-01-03 14:20:00 -0500

    new task form

--- gantts-frontend/src/app.js
+++ gantts-frontend/src/app.js
@@ -1,16 +1,34 @@
 class App {
 
-  static doubleClick() {
-    document.addEventListener('dblclick', (e)=>{
+  static click() {
+    document.addEventListener('click', (e) => {
 
-  })
+      switch (e.target.className) {
+        case "track ui-sortable":
+          let createTaskBtn = document.createElement('BUTTON')
+          createTaskBtn.className = "create-task-button"
+          createTaskBtn.innerHTML = "Create New Task!"
 
-  }
+          let newTask = document.createElement('DIV')
+          newTask.className = "task"
+          newTask.appendChild(createTaskBtn)
+          e.target.append(newTask)
+          break
+
+        case "create-task-button":
+          let newForm = TaskForm.newForm()
+          e.target.parentElement.append(newForm)
+          e.target.remove()
+          break
+
+        case "create-task":
+          let taskTitle = document.getElementById('task-title').value
+          let taskContent = document.getElementById('task-content').value
+          Task.create(taskTitle, taskContent)
+      }
+
+    })//CLICK-EVENTLISTENER
+  }//CLICK FUCNTION
 
 
-  static singleClick() {
-    document.body.addEventListener('click', (e)=>{
-      alert()
-    })
-  }
 }
--- gantts-frontend/src/index.js
+++ gantts-frontend/src/index.js
@@ -1,10 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
   // ProjectsAdapter.all()
-<<<<<<< HEAD
 
-  App.doubleClick()
-=======
-  App.singleClick()
+  App.click()
 
->>>>>>> 5b188382f5fe6b21382363082876216b0b80eca1
 })

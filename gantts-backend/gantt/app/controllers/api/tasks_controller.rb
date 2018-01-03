3173d11c9c53a49b88e0ef5bd2bffe03be02a186
github username <github email address>
2018-01-03 14:54:04 -0500

    create task funtion

--- gantts-backend/gantt/app/controllers/api/tasks_controller.rb
+++ gantts-backend/gantt/app/controllers/api/tasks_controller.rb
@@ -5,4 +5,15 @@ class Api::TasksController < ApplicationController
     render json: @task, status: 200
   end
 
+  def create
+    @task = Task.new(title: params[:title], content: params[:content], start_time: params[:start_time], duration: params[:duration])
+
+    if @task.valid?
+      @task.save
+      render json: @task, status: 200
+    else
+      render message: "Ya blew it!"
+    end
+  end
+
 end
